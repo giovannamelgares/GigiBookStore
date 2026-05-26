419ba60bb93b6812502ff9b7f82ceea844af919d
Giovanna Melgares <giovannamelgares222@gmail.com>
2026-05-25 22:45:59 -0300

    arrumar cadastro

--- src/main/java/com/example/sitep2livraria/model/UsuarioDAO.java
+++ src/main/java/com/example/sitep2livraria/model/UsuarioDAO.java
@@ -1,19 +1,22 @@
 package com.example.sitep2livraria.model;
 
-import javax.sql.DataSource;
-
 import java.util.Map;
 import java.util.UUID;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
+
 import jakarta.annotation.PostConstruct;
 
 @Repository
 public class UsuarioDAO {
+
     @Autowired
     DataSource dataSource;
 
@@ -30,28 +33,45 @@ public class UsuarioDAO {
     }
 
     public void inserirUsuario(Usuario usuario) {
+
+        UUID uuid = UUID.randomUUID();
+
         String sql =
-            "INSERT INTO usuario(nome,email,password) " + "VALUES (?,?,?)";
-        Object[] obj = new Object[3];
-        obj[0] = usuario.getNome();
-        obj[1] = usuario.getEmail();
+            "INSERT INTO usuario(id,nome,email,password) VALUES (?,?,?,?)";
+
+        Object[] obj = new Object[4];
+
+        obj[0] = uuid;
+        obj[1] = usuario.getNome();
+        obj[2] = usuario.getEmail();
+
         PasswordEncoder pe = passwordEncoder();
-        obj[2] = pe.encode(usuario.getPassword());
+        obj[3] = pe.encode(usuario.getPassword());
+
         jdbc.update(sql, obj);
+
+        inserirPerfil(uuid.toString());
     }
 
     public String obterUUID(String email){
         String sql = "SELECT id FROM usuario WHERE email=?";
         Map<String,Object> mp = jdbc.queryForMap(sql,email);
+
         UUID uuid = (UUID) mp.get("id");
+
         return uuid.toString();
     }
 
     public void inserirPerfil(String uuid){
-        String sql = "INSERT INTO perfil(usuarioid, cargo) " + "VALUES (?, ?)";
-        Object[] obj = new Object[2];  
-        obj[0] = uuid;
+
+        String sql =
+            "INSERT INTO perfil(usuarioid, cargo) VALUES (?, ?)";
+
+        Object[] obj = new Object[2];
+
+        obj[0] = UUID.fromString(uuid);
         obj[1] = "cliente";
+
         jdbc.update(sql, obj);
     }
 }
